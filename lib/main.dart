7d4f0f275c2c3412dacbaa3bf5fe9c8cc7dedcc8
Ibrahim Kayyis <kayeekid@gmail.com>
2026-08-21 15:56:00 +0700

    fix(ci): add --no-fatal-infos flag to analyze step and clean up linter warnings
    - In .github/workflows/ci.yml:
      * Add --no-fatal-infos flag (Flutter 3.35.4 enables --fatal-infos by default, causing exit code 1 on non-fatal info lints)
    - In lib/main.dart:
      * Remove unused imports (firebase_app_check, flutter/foundation)
    - In pubspec.yaml:
      * Remove duplicate flutter_native_splash from dev_dependencies
    - In target_hafalan_helper.dart:
      * Add curly braces to if-else branches (curly_braces_in_flow_control_structures)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'dart:async';
 
-import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
